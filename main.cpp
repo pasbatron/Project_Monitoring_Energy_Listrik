0394c8052690f1ce0ef21eb04fdf47522f59a861
Pasbatron <131593509+pasbatron@users.noreply.github.com>
2024-05-23 07:07:26 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
+#include <LiquidCrystal_I2C.h>
 #include <ModbusRTU.h>
 #include <SoftwareSerial.h>
 
+
 #define TX_G16 16
 #define RX_G17 17
-#define RE_DE_ENABLE 15
+
+#define TX_G26 26
+#define RX_G27 27
+int lcdColumns = 16;
+int lcdRows = 2;
+// #define RE_DE_ENABLE 15
 
 float InttoFloat(uint16_t Data0, uint16_t Data1){
   float x;
@@ -13,8 +20,10 @@ float InttoFloat(uint16_t Data0, uint16_t Data1){
   *p = (unsigned long)Data0 << 16 | Data1; //Bid-endian
   return (x);
 }
-SoftwareSerial S(RX_G17, TX_G16); // RX, TX
+SoftwareSerial S(RX_G27, TX_G26); // RX, TX
+SoftwareSerial mySerialLora(TX_G16, RX_G17); //TX, RX
 ModbusRTU mb;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
 
 bool cb(Modbus::ResultCode event, uint16_t transactionId, void* data){
   if(event != Modbus::EX_SUCCESS){
@@ -24,36 +33,41 @@ bool cb(Modbus::ResultCode event, uint16_t transactionId, void* data){
   return true;
 }
 
-
 void setup() {
   Serial.begin(9600);
+  mySerialLora.begin(9600);
   S.begin(19200, SWSERIAL_8E1);
-  mb.begin(&S,RE_DE_ENABLE);
+  mb.begin(&S);
   mb.master();
-
+  lcd.init();                    
+  lcd.backlight();
 }
 
-uint16_t val[2];
-
+uint16_t data[2];
 void loop() {
-  
-  if (!mb.slave()) {
-    
-    mb.readHreg(2, 3926, val, 2, cb); // Slave id is 1 and register address is 3926 and 
-    //we are reading 2 bytes from the register and saving in val
-    while(mb.slave()) { // Check if transaction is active
+
+  if (!mb.slave()){
+    mb.readHreg(2, 3960, data, 2, cb);
+    while(mb.slave()) {
       mb.task();
       delay(100);
     }
-    Serial.print("Register Values ");
-    Serial.print(val[0]);
-    Serial.print(",");
-    Serial.println(val[1]);
-    float voltage= InttoFloat(val[1],val[0]);
-    Serial.print("Voltage= ");
-    Serial.print(voltage);
-    Serial.println(" V");
 
-      }
-  delay(1000);
+    Serial.print("Register Values ");
+    Serial.print(data[0]);
+    Serial.print(",");
+    Serial.println(data[1]);
+
+    float Data= InttoFloat(data[1],data[0]);
+    Serial.print("Data= ");
+    Serial.print(Data);
+    Serial.println(" Satuan");
+    mySerialLora.println("line-cam-housing;" + String(Data) + ";");
+    lcd.setCursor(0, 0);
+    lcd.print("Data : ");
+    lcd.setCursor(0,1);
+    lcd.print(Data);
+  }
+  delay(10000);
+  lcd.clear();
 }
